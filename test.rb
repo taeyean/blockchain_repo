dc14224ffb2afa4830036cca2fe481b505a7d75f
taeyean <kte4567@keypair.co.kr>
2018-04-23 22:03:08 +0900

    a

---
+++ test.rb
@@ -0,0 +1,8 @@
+# encoding: UTF-8
+
+require 'sinatra'
+require "sinatra/reloader"
+
+get '/' do
+   "block!!"
+end
---
+++ test1.rb
@@ -0,0 +1,18 @@
+# encoding: UTF-8
+
+require 'sinatra'
+require "sinatra/reloader"
+
+get '/taeyean' do
+   'Put this in your pipe & smoke it!'
+end
+
+get '/mine' do
+  '<h1 align="center">you mine a block</h1>
+  <p align="center"> by taeyean </p>'
+end
+
+get '/hello' do
+  '<h1 align="center">you must say hello</h1>
+  <p align="center"> by taeyean </p>'
+end
