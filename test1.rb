16ba509f97ff9df3449b76413fe882b550b80f89
taeyean <kte4567@keypair.co.kr>
2018-04-23 22:09:15 +0900

    add refresh page

--- test1.rb
+++ test1.rb
@@ -16,3 +16,8 @@ get '/hello' do
   '<h1 align="center">you must say hello</h1>
   <p align="center"> by taeyean </p>'
 end
+
+get '/refresh' do
+  '<h1 align="center">you have to take a rest!!</h1>
+  <p align="center"> by taeyean </p>'
+end
