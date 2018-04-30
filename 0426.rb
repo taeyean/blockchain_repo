16f52d64d98317d463a6693490d8744d32ab2a89
taeyean <kte4567@keypair.co.kr>
2018-04-30 22:49:16 +0900

    wallet_function
    1. add transaction
    2. add mkWallet
    3. add handling amount

--- 0426.rb
+++ 0426.rb
@@ -11,6 +11,7 @@ get '/' do
 	message << "the time is " + c['time'].to_s + "<br>"
 	message << "last address is " + c['previous_block'].to_s + "<br>"
 	message << "my address is " + Digest::SHA256.hexdigest(c.to_s) + "<br>"
+	message << "transaction " + c['transaction'].to_s + "<br>"
 	message << "<hr>"
   end
   
@@ -20,4 +21,18 @@ end
 get '/mine' do
   b.mining.to_s
   "I found blocks"
+end
+ 
+get '/transaction' do
+  "sender: "+params["sender"]+"<br>receiver: "+params["receiver"]+"<br>amount: "+params["amount"]
+  b.trans(params["sender"], params["receiver"], params["amount"])
+  #"Done"
+end
+
+get '/mkWallet' do
+  b.mkWallet.to_s
+end
+
+get '/allWallet' do
+  b.show_allWallet.to_s
 end
--- block.rb
+++ block.rb
@@ -1,23 +1,62 @@
 require 'digest'
+require 'securerandom'
 
 class Blockchain
 
   def initialize
+  
     @chain = ['block1',]
+	@transaction = []
+	@wallet = {}
+  end
+  
+  def mkWallet
+    #if @wallet == {}
+	  newWallet_addr = SecureRandom.uuid
+	  @wallet[newWallet_addr] = 1000
+	#else
+	#  @wallet
+	#end
   end
 
+  def trans(s, r, a)
+    if @wallet[s].nil?
+	  "[snd] This wallet did not exist"
+	elsif @wallet[r].nil?
+	  "[rcv] This wallet did not exist"
+    elsif @wallet[s] < a.to_f
+	  "[snd] Not Enought Amount"
+	else  
+	  transaction = {
+  	    "sender" => s,
+	    "receiver" => r,
+	    "amount" => a
+	  }
+	  @wallet[r] = @wallet[r] + a.to_f
+	  @wallet[s] = @wallet[s] - a.to_f
+	  @transaction << transaction
+	  "Done"
+	end
+  end
+  
+  def show_allWallet
+    @wallet
+  end
+  
   def mining
 	begin
 	  nonce = rand(100)
 	end while nonce != 0
 	
 	block = {
-	  'index' => @chain.length + 1,
-	  'time' => Time.now,
-	  'nonce' => nonce,
-	  'previous_block' => Digest::SHA256.hexdigest(last_block.to_s)
+	  "nonce" => nonce,
+	  "index" => @chain.length + 1,
+	  "previous_block" => Digest::SHA256.hexdigest(last_block.to_s),
+	  "time" => Time.now,
+	  "transaction" => @transaction
 	}
 	@chain << block
+	@transaction = []
   end
   
   def current_chain
